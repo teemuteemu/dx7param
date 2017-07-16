e61ea1555c73b242e6be95a77fa0ffa900f1269b
Teemu K <teemu.tuomas.oskari@gmail.com>
2017-07-16 14:02:38 +0000

    Preparing for voice change messages

--- midi.js
+++ midi.js
@@ -5,11 +5,24 @@ const params = require('./param.js');
 const STATUS_START_SYSEX = 0xF0;
 const STATUS_END_SYSEX = 0xF7;
 const STATUS_ID_YAMAHA = 0x43;
-const STATUS_SUB_CH = 0x10;
-const HEADER = [
+const STATUS_SUB_PARAM_CHANGE = 0x10;
+const STATUS_SUB_VOICE_CHANGE = 0x00;
+const STATUS_VOICE_FORMAT_1 = 0x00;
+const STATUS_VOICE_CHANGE_1_MSB = 0x01;
+const STATUS_VOICE_CHANGE_1_LSB = 0x1B;
+
+const PARAM_CHANGE_HEADER = [
   STATUS_START_SYSEX,
   STATUS_ID_YAMAHA,
-  STATUS_SUB_CH
+  STATUS_SUB_PARAM_CHANGE
+];
+const VOICE_CHANGE_HEADER = [
+  STATUS_START_SYSEX,
+  STATUS_ID_YAMAHA,
+  STATUS_SUB_VOICE_CHANGE,
+  STATUS_VOICE_FORMAT_1,
+  STATUS_VOICE_CHANGE_1_MSB,
+  STATUS_VOICE_CHANGE_1_LSB
 ];
 const FOOTER = [
   STATUS_END_SYSEX
@@ -17,9 +30,10 @@ const FOOTER = [
 
 const MIDI_DEV_ID = 1; // TODO bad magic #
 
-function createMessage (parameter, value) {
+function createParamChangeMessage (parameter, value) {
   const param = params.toDxValue(parameter, value);
-  const payload = HEADER
+  console.log(param);
+  const payload = PARAM_CHANGE_HEADER
     .concat(param)
     .concat(FOOTER);
 
@@ -37,8 +51,8 @@ function initMidi () {
 
 function sendParam (output, param, value) {
   try {
-    const message = createMessage(param, value);
-    // console.log(`Sending ${message}...`);
+    const message = createParamChangeMessage(param, value);
+    // console.log(`Sending parameter: "${message}"`);
     output.sendMessage(message);
   } catch (e) {
     console.log('Bad param');
